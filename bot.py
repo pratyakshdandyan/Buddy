db1c05785c6388608fbaec9127db7eccc8cdcfd9
pratyakshdandyan <44299452+pratyakshdandyan@users.noreply.github.com>
2018-10-19 22:26:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,22 +2,239 @@ import discord
 import asyncio
 import youtube_dl
 import os
-from discord.ext import command
-from discord.ext.comands import bot
+from discord.ext import commands
+from discord.ext.commands import Bot
 
 
-bot=commands.Bot(command_prefix='!b')
+bot=commands.Bot(command_prefix='.')
+
+from discord import opus
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll',
+             'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
 
 
+def load_opus_lib(opus_libs=OPUS_LIBS):
+    if opus.is_loaded():
+        return True
 
+    for opus_lib in opus_libs:
+            try:
+                opus.load_opus(opus_lib)
+                return
+            except OSError:
+                pass
+
+    raise RuntimeError('Could not load an opus lib. Tried %s' %
+                       (', '.join(opus_libs)))
+load_opus_lib()
+
+in_voice=[]
+
+
+players = {}
+songs = {}
+playing = {}
+
+
+async def all_false():
+    for i in bot.servers:
+        playing[i.id]=False
+
+
+async def checking_voice(ctx):
+    await asyncio.sleep(130)
+    if playing[ctx.message.server.id]== False:
+        try:
+            pos = in_voice.index(ctx.message.server.id)
+            del in_voice[pos]
+            server = ctx.message.server
+            voice_client = bot.voice_client_in(server)
+            await voice_client.disconnect()
+            await bot.say("{} left because there was no audio playing for a while".format(bot.user.name))
+        except:
+            pass
 
 @bot.event
 async def on_ready():
-     bot.loop.create_task(all_flase())
-     await bot.change_presence(game=discord.Game(name='buddy'))
-     print(bot.user.name)
-     
-     
+   bot.loop.create_task(all_false())
+   await bot.change_presence(game=discord.Game(name='Pnada ka beta'))
+   print(bot.user.name)
+    
+@bot.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await bot.join_voice_channel(channel)
+    in_voice.append(ctx.message.server.id)
+
+
+async def player_in(con):  # After function for music
+    try:
+        if len(songs[con.message.server.id]) == 0:  # If there is no queue make it False
+            playing[con.message.server.id] = False
+            bot.loop.create_task(checking_voice(con))
+    except:
+        pass
+    try:
+        if len(songs[con.message.server.id]) != 0:  # If queue is not empty
+            # if audio is not playing and there is a queue
+            songs[con.message.server.id][0].start()  # start it
+            await bot.send_message(con.message.channel, '```Now queueed```')
+            del songs[con.message.server.id][0]  # delete list afterwards
+    except:
+        pass
+
+
+@bot.command(pass_context=True)
+async def play(ctx, *,url):
+
+    opts = {
+        'default_search': 'auto',
+        'quiet': True,
+    }  # youtube_dl options
+
+
+    if ctx.message.server.id not in in_voice: #auto join voice if not joined
+        channel = ctx.message.author.voice.voice_channel
+        await bot.join_voice_channel(channel)
+        in_voice.append(ctx.message.server.id)
+
+    
+
+    if playing[ctx.message.server.id] == True: #IF THERE IS CURRENT AUDIO PLAYING QUEUE IT
+        voice = bot.voice_client_in(ctx.message.server)
+        song = await voice.create_ytdl_player(url, ytdl_options=opts, after=lambda: bot.loop.create_task(player_in(ctx)))
+        songs[ctx.message.server.id]=[] #make a list 
+        songs[ctx.message.server.id].append(song) #add song to queue
+        await bot.say("```Audio {} is queued```".format(song.title))
+
+    if playing[ctx.message.server.id] == False:
+        voice = bot.voice_client_in(ctx.message.server)
+        player = await voice.create_ytdl_player(url, ytdl_options=opts, after=lambda: bot.loop.create_task(player_in(ctx)))
+        players[ctx.message.server.id] = player
+        # play_in.append(player)
+        if players[ctx.message.server.id].is_live == True:
+            await bot.say("Can not play live audio yet.")
+        elif players[ctx.message.server.id].is_live == False:
+            player.start()
+            await bot.say("```Now playing audio```")
+            playing[ctx.message.server.id] = True
+
+
+
+@bot.command(pass_context=True)
+async def queue(con):
+    await bot.say("```There are currently {} audios in queue```".format(len(songs)))
+
+@bot.command(pass_context=True)
+async def pause(ctx):
+    id = ctx.message.server.id
+    players[id].pause()
+
+@bot.command(pass_context=True)
+async def resume(ctx):
+    players[ctx.message.server.id].resume()
+          
+@bot.command(pass_context=True)
+async def volume(ctx, vol:float):
+    volu = float(vol)
+    players[ctx.message.server.id].volume=volu
+
+
+@bot.command(pass_context=True)
+async def skip(con): #skipping songs?
+  songs[con.message.server.id]
+    
+    
+    
+@bot.command(pass_context=True)
+async def stop(con):
+    players[con.message.server.id].stop()
+    songs.clear()
+
+@bot.command(pass_context=True)
+async def leave(ctx):
+    pos=in_voice.index(ctx.message.server.id)
+    del in_voice[pos]
+    server=ctx.message.server
+    voice_client=bot.voice_client_in(server)
+    await voice_client.disconnect()
+    songs.clear()
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await bot.say(":ping_pong: ping!! xSSS")
+    print ("user has pinged")
+
+@bot.command(pass_context=True)
+async def info(ctx, user: discord.Member):
+    embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what I could find.", color=0x00ff00)
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="ID", value=user.id, inline=True)
+    embed.add_field(name="Status", value=user.status, inline=True)
+    embed.add_field(name="Highest role", value=user.top_role)
+    embed.add_field(name="Joined", value=user.joined_at)
+    embed.set_thumbnail(url=user.avatar_url)
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def serverinfo(ctx):
+    embed = discord.Embed(name="{}'s info".format(ctx.message.server.name), description="Here's what I could find.", color=0x00ff00)
+    embed.set_author(name="Will Ryan of DAGames")
+    embed.add_field(name="Created at", value=ctx.message.server.created_at, inline=True)
+    embed.add_field(name="Owner", value=ctx.message.server.owner, inline=True)
+    embed.add_field(name="Name", value=ctx.message.server.name, inline=True)
+    embed.add_field(name="ID", value=ctx.message.server.id, inline=True)
+
+    
+    embed.add_field(name="AFK channel", value=ctx.message.server.afk_channel, inline=True)
+    embed.add_field(name="Verification", value=ctx.message.server.verification_level, inline=True)
+    embed.add_field(name="Region", value=ctx.message.server.region, inline=True)
+    embed.add_field(name="Roles", value=len(ctx.message.server.roles), inline=True)
+    embed.add_field(name="Members", value=len(ctx.message.server.members))
+    embed.add_field(name="Emojis", value=ctx.message.server.emojis, inline=True)
+    embed.set_thumbnail(url=ctx.message.server.icon_url)
+    await bot.say(embed=embed)    
+      
 @bot.command(pass_context=True)
 
+async def kick(ctx, user: discord.Member):
+    await bot.say(":boot: Cya, {}. Ya loser!".format(user.name))
+    await bot.kick(user)
+ 
+     
+     
 
+
+
+
+    
+    
+@bot.command(pass_context=True)
+async def embed(ctx):
+    embed = discord.Embed(title="test", description="my name imran", color=0x00ff00)
+    embed.set_footer(text="this is a footer")
+    embed.set_author(name="Will Ryan of DAGames")
+    embed.add_field(name="This is a field", value="no it isn't", inline=True)
+    await bot.say(embed=embed)
+   
+
+
+
+
+   
+  
+
+
+   
+   
+   
+    
+
+
+  
+
+
+
+
+    
+bot.run(os.environ['BOT_TOKEN'])

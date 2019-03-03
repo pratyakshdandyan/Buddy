fde656a75350639a74711e36084d53349d97aea8
Iamanerror <43728688+Iamanerror@users.noreply.github.com>
2019-03-03 21:52:38 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -463,6 +463,8 @@ async def on_reaction_add(reaction, user):
     embed.add_field(name = "avatar", value="b.avatar @user [show user avatar",inline = False)
     embed.add_field(name = "guildcount", value="b.guildcount",inline = False)
     embed.add_field(name = "guildid", value="b.guildid",inline = False)
+    embed.add_field(name = 'botinfo',value ='Use it like ``b.botinfo`` to get some basic info of bot',inline = False)
+    embed.add_field(name = 'membercount', value='Use it like ``b.membercount`` to see how many members are in the server.', inline = False)	
     await bot.send_message(user,embed=embed)
 
   if reaction.emoji == '🇲':
@@ -476,7 +478,15 @@ async def on_reaction_add(reaction, user):
     embed.add_field(name = 'clear(Manage Messages Permission Required)',value ='Use it like ``b.clear <number>`` to clear any message',inline = False)
     embed.add_field(name = 'mute(Mute members Permission Required)',value ='Use it like ``b.mute @user <time>`` to mute any user',inline = False)
     embed.add_field(name = 'unmute(Mute members Permission Required) ',value ='Use it like ``b.unmute @user`` to unmute anyone',inline = False)
-    embed.add_field(name = "unban", value="b.unban user.id | for example d!unban 277983178914922497",inline = False)
+    embed.add_field(name = "unban", value="b.unban user.id | for example b.unban 277983178914922497",inline = False)
+    embed.add_field(name = 'setupwelcomer(Admin Permission required)',value ='Simply use it to make a channel named welcome so that bot will send welcome and leaves logs in that channel.',inline = False)
+    embed.add_field(name = 'setuplog(Admin Permission required)',value ='Simply use it to make a channel named logs so that bot will send logs in that channel.',inline = False)
+    embed.add_field(name = 'Dm(Admin Permission required)', value="Use it like ``b.dm @user <text>`` to send dm any one",inline = False)
+    embed.add_field(name = 'say(Admin permission required)',value ='Use it like ``b.say <text>``',inline = False)
+    embed.add_field(name = 'announce(Admin permission required)', value="Use it like ``b.announce #channel <text>``",inline = False)
+
+
+
     await bot.send_message(user,embed=embed)
   
   if reaction.emoji == '🏵':
@@ -486,6 +496,14 @@ async def on_reaction_add(reaction, user):
     embed.set_image(url = 'https://image.ibb.co/caM2BK/help.gif')
     embed.add_field(name = "dice", value="50 50 chance")
     embed.add_field(name = "coinflip", value="50 50 chance of getting tails and heads.")
+    embed.add_field(name = "kiss", value="Use it like ``b.kiss @user``")
+    embed.add_field(name = "hug", value="Use it like ``b.hug @user``")
+    embed.add_field(name = "slap", value="Use it like ``b.slap @user``")
+    embed.add_field(name = "rolldice", value="Use it like ``b.rolldice`` [fun command]")
+    embed.add_field(name = "filpcoin", value="Use it like ``b.flipcoin`` [50 50 chance]")
+    embed.add_field(name = 'joke',value ='Use it like ``b.joke`` to get a random joke')
+    embed.add_field(name = "randomshow", value="Use it like ``b.randomshow`` [fun command]")
+    embed.add_field(name = 'Note:', value="**More commands being added soon!**")
     await bot.send_message(user,embed=embed)
     
  
@@ -578,6 +596,285 @@ async def dice( con, min1=1, max1=6):
     MAX1 = THE LIMIT TO GENERATE A RANDOM NUMBER"""
     r = random.randint(min1, max1)
     await bot.send_message(con.message.channel, "**{}**".format(r))
+
+
+@bot.event
+async def on_message_delete(message):
+    if not message.author.bot:
+      channelname = 'logs'
+      logchannel=None
+      for channel in message.server.channels:
+        if channel.name == channelname:
+          user = message.author
+      for channel in message.author.server.channels:
+        if channel.name == 'logs':
+          logchannel = channel
+          r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+          embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+          embed.set_author(name='Message deleted')
+          embed.add_field(name = 'User: **{0}**'.format(user.name),value ='UserID: **{}**'.format(user.id),inline = False)
+          embed.add_field(name = 'Message:',value ='{}'.format(message.content),inline = False)
+          embed.add_field(name = 'Channel:',value ='{}'.format(message.channel.name),inline = False)
+          await bot.send_message(logchannel,  embed=embed)
+
+
+@bot.command(name="say", pass_context=True)
+@commands.has_permissions(administrator=True)
+async def _say(ctx, *, msg = None):
+    await bot.delete_message(ctx.message)
+
+    if not msg: await bot.say("Please specify a message to send")
+    else: await bot.say(msg)
+    return
+    
+    embed = discord.Embed(title=f"User: {ctx.message.author.name} have used say command", description=f"ID: {ctx.message.author.id}", color=0xff9393)
+    await bot.send_message(channel, embed=embed)
+	
+
+@_say.error
+async def say_error(error, ctx):
+	if isinstance(error, discord.ext.commands.errors.CheckFailure):
+		text = "Sorry {}, you do not have a administrator permission to use this command.".format(ctx.message.author.mention)
+		await bot.send_message(ctx.message.channel, text)
+		
+		
+		
+@bot.command(pass_context=True, no_pm=True)
+async def membercount(ctx):
+	members = set(ctx.message.server.members)
+	bots = filter(lambda m: m.bot, members)
+	bots = set(bots)
+	users = members - bots
+	await bot.send_message(ctx.message.channel, embed=discord.Embed(title="Membercount", description="{} there is {} users and {} bots with a total of {} members in this server.".format(ctx.message.author.mention, len(users), len(bots), len(ctx.message.server.members)), colour=0X008CFF))
+	embed = discord.Embed(title=f"User: {ctx.message.author.name} have used membercount command", description=f"ID: {ctx.message.author.id}", color=0xff9393)
+	await bot.send_message(channel, embed=embed)		
+		
+		
+		
+@bot.command(pass_context=True)
+async def slap(ctx, user: discord.Member = None):
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    gifs = ["http://rs20.pbsrc.com/albums/b217/strangething/flurry-of-blows.gif?w=280&h=210&fit=crop", "https://media.giphy.com/media/LB1kIoSRFTC2Q/giphy.gif", "https://i.imgur.com/4MQkDKm.gif"]
+    if user == None:
+        await bot.say(f"{ctx.message.author.mention} ```Proper usage is\n\n>slap <mention a user>```")
+    else:
+        embed = discord.Embed(title=f"{ctx.message.author.name} Just slapped the shit out of {user.name}!", color = discord.Color((r << 16) + (g << 8) + b))
+        embed.set_image(url=random.choice(gifs))
+        await bot.say(embed=embed)	
+		
+		
+		
+@bot.command(pass_context=True)
+async def kiss(ctx, user: discord.Member):
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    randomurl = ["https://media3.giphy.com/media/G3va31oEEnIkM/giphy.gif", "https://i.imgur.com/eisk88U.gif", "https://media1.tenor.com/images/e4fcb11bc3f6585ecc70276cc325aa1c/tenor.gif?itemid=7386341", "http://25.media.tumblr.com/6a0377e5cab1c8695f8f115b756187a8/tumblr_msbc5kC6uD1s9g6xgo1_500.gif"]
+    if user.id == ctx.message.author.id:
+        await bot.say("Goodluck kissing yourself {}".format(ctx.message.author.mention))
+    else:
+        embed = discord.Embed(title=f"{user.name} You just got a kiss from {ctx.message.author.name}", color = discord.Color((r << 16) + (g << 8) + b))
+        embed.set_image(url=random.choice(randomurl))
+        await bot.say(embed=embed)
+
+
+
+@bot.command(pass_context=True)
+async def hug(ctx, user: discord.Member):
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    if user.id == ctx.message.author.id:
+        await bot.say("{} Wanted to hug himself/herself , good luck on that you will look like an idiot trying to do it".format(user.mention))
+    else:
+        randomurl = ["http://gifimage.net/wp-content/uploads/2017/09/anime-hug-gif-5.gif", "https://media1.tenor.com/images/595f89fa0ea06a5e3d7ddd00e920a5bb/tenor.gif?itemid=7919037", "https://media.giphy.com/media/NvkwNVuHdLRSw/giphy.gif"]
+        embed = discord.Embed(title=f"{user.name} You just got a hug from {ctx.message.author.name}", color = discord.Color((r << 16) + (g << 8) + b))
+        embed.set_image(url=random.choice(randomurl))
+        await bot.say(embed=embed)
+	
+	
+@bot.command(pass_context=True)
+async def joke(ctx):
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    joke = ["What do you call a frozen dog?\nA pupsicle", "What do you call a dog magician?\nA labracadabrador", "What do you call a large dog that meditates?\nAware wolf", "How did the little scottish dog feel when he saw a monster\nTerrier-fied!", "Why did the computer show up at work late?\nBecause it had a hard drive", "Autocorrect has become my worst enime", "What do you call an IPhone that isn't kidding around\nDead Siri-ous", "The guy who invented auto-correct for smartphones passed away today\nRestaurant in peace", "You know you're texting too much when you say LOL in real life, instead of laughing", "I have a question = I have 18 Questions\nI'll look into it = I've already forgotten about it", "Knock Knock!\nWho's there?\Owls say\nOwls say who?\nYes they do.", "Knock Knock!\nWho's there?\nWill\nWill who?\nWill you just open the door already?", "Knock Knock!\nWho's there?\nAlpaca\nAlpaca who?\nAlpaca the suitcase, you load up the car.", "Yo momma's teeth is so yellow, when she smiled at traffic, it slowed down.", "Yo momma's so fat, she brought a spoon to the super bowl.", "Yo momma's so fat, when she went to the beach, all the whales started singing 'We are family'", "Yo momma's so stupid, she put lipstick on her forehead to make up her mind.", "Yo momma's so fat, even Dora can't explore her.", "Yo momma's so old, her breast milk is actually powder", "Yo momma's so fat, she has to wear six different watches: one for each time zone", "Yo momma's so dumb, she went to the dentist to get a bluetooth", "Yo momma's so fat, the aliens call her 'the mothership'", "Yo momma's so ugly, she made an onion cry.", "Yo momma's so fat, the only letters she knows in the alphabet are K.F.C", "Yo momma's so ugly, she threw a boomerang and it refused to come back", "Yo momma's so fat, Donald trump used her as a wall", "Sends a cringey joke\nTypes LOL\nFace in real life : Serious AF", "I just got fired from my job at the keyboard factory. They told me I wasn't putting enough shifts.", "Thanks to autocorrect, 1 in 5 children will be getting a visit from Satan this Christmas.", "Have you ever heard about the new restaurant called karma?\nThere's no menu, You get what you deserve.", "Did you hear about the claustrophobic astronaut?\nHe just needed a little space", "Why don't scientists trust atoms?\nBecase they make up everything", "How did you drown a hipster?\nThrow him in the mainstream", "How does moses make tea?\nHe brews", "A man tells his doctor\n'DOC, HELP ME. I'm addicted to twitter!'\nThe doctor replies\n'Sorry i don't follow you...'", "I told my wife she was drawing her eyebrows too high. She looked surprised.", "I threw a boomeranga a few years ago. I now live in constant fear"]
+    embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+    embed.add_field(name=f"Here is a random joke that {ctx.message.author.name} requested", value=random.choice(joke))
+    await bot.say(embed=embed)		
+		
+@bot.command(pass_context=True)
+async def burned(ctx):
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+    embed.set_image(url="https://i.imgur.com/wY4xbak.gif")
+    await bot.say(embed=embed)
+    await bot.delete_message(ctx.message)
+
+
+
+
+
+@bot.command(pass_context = True)
+async def dm(ctx, user: discord.Member, *, msg: str):
+   if user is None or msg is None:
+       await bot.say('Invalid args. Use this command like: ``d?dm @user message``')
+   if ctx.message.author.server_permissions.kick_members == False:
+       await bot.say('**You do not have permission to use this command**')
+       return
+   else:
+       await bot.send_message(user, msg)
+       await bot.delete_message(ctx.message)          
+       await bot.say("Success! Your DM has made it! :white_check_mark: ")
+
+
+@bot.command(pass_context = True)
+async def flipcoin(ctx):
+    choices = ['Heads', 'Tails', 'Coin self-destructed']
+    color = discord.Color(value=0x00ff00)
+    embed=discord.Embed(color=color, title='Flipped a coin!')
+    embed.description = random.choice(choices)
+    await bot.send_typing(ctx.message.channel)
+    await bot.say(embed=embed)	
+	
+	
+@bot.command(pass_context = True)
+async def rolldice(ctx):
+    choices = ['1', '2', '3', '4', '5', '6']
+    color = discord.Color(value=0x00ff00)
+    em = discord.Embed(color=color, title='Rolled! (1 6-sided die)', description=random.choice(choices))
+    await bot.send_typing(ctx.message.channel)
+    await bot.say(embed=em)
+
+
+
+
+@bot.command(pass_context = True)
+async def announce(ctx, channel: discord.Channel=None, *, msg: str=None):
+    member = ctx.message.author
+    if channel is None or msg is None:
+        await bot.say('Invalid args. Use this command like ``d?announce #channel text here``')
+        return
+    else:
+        if member.server_permissions.administrator == False:
+            await bot.say('**You do not have permission to use this command**')
+            return
+        else:
+            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+            embed=discord.Embed(title="Announcement", description="{}".format(msg), color = discord.Color((r << 16) + (g << 8) + b))
+            await bot.send_message(channel, embed=embed)
+            await bot.delete_message(ctx.message)
+
+	
+@bot.event
+async def on_message_edit(before, after):
+    if before.content == after.content:
+      return
+    if before.author == bot.user:
+      return
+    else:
+      user = before.author
+      member = after.author
+      for channel in user.server.channels:
+        if channel.name == 'logs':
+            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+            embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+            embed.set_author(name='Message edited')
+            embed.add_field(name = 'User: **{0}**'.format(user.name),value ='UserID: **{}**'.format(user.id),inline = False)
+            embed.add_field(name = 'Before:',value ='{}'.format(before.content),inline = False)
+            embed.add_field(name = 'After:',value ='{}'.format(after.content),inline = False)
+            embed.add_field(name = 'Channel:',value ='{}'.format(before.channel.name),inline = False)
+            await bot.send_message(channel, embed=embed)
+		
+		
+		
+@bot.command(pass_context = True)
+async def setupwelcomer(ctx):
+    if ctx.message.author.bot:
+      return
+    if ctx.message.author.server_permissions.administrator == False:
+      await bot.say('**You do not have permission to use this command**')
+      return
+    else:
+      server = ctx.message.server
+      everyone_perms = discord.PermissionOverwrite(send_messages=False, read_messages=True)
+      everyone = discord.ChannelPermissions(target=server.default_role, overwrite=everyone_perms)
+      await bot.create_channel(server, 'welcome',everyone)
+      await bot.say(':white_check_mark:**Success setup**')		
+		
+		
+@bot.command(pass_context = True)
+async def setuplog(ctx):
+    if ctx.message.author.bot:
+      return
+    if ctx.message.author.server_permissions.administrator == False:
+      await bot.say('**You do not have permission to use this command**')
+      return
+    else:
+      author = ctx.message.author
+      server = ctx.message.server
+      everyone_perms = discord.PermissionOverwrite(send_messages=False, read_messages=True)
+      everyone = discord.ChannelPermissions(target=server.default_role, overwrite=everyone_perms)
+      await bot.create_channel(server, 'logs',everyone)	
+      await bot.say(':white_check_mark:**Success setup**')	
+		
+		
+rq = requests
+bot.command(pass_context=True)
+async def mal(ctx):
+        session = rq.Session()
+        """SEARCH FOR ANIME USING MyAnimeList. EX: s.mal Mushishi"""
+        query = ctx.message.content[5:]
+        url = 'https://api.jikan.moe/search/anime/{}/'.format(query)
+        rq_url = session.get(url).text
+        rq_json = json.loads(rq_url)
+        anime_id = rq_json['result'][0]['mal_id']
+        url2 = 'https://api.jikan.moe/anime/{}/stats/'.format(anime_id)
+        rq_url2 = session.get(url2).text
+        rq_json2 = json.loads(rq_url2)
+        summary = rq_json2['synopsis']
+        title_jp = rq_json2['title_japanese']
+        title_en = rq_json2['title_english']
+        anime_type = rq_json2['type']
+        status = rq_json2['status']
+        aired_from = rq_json2['aired']['from']
+        members = rq_json2['members']
+        popularity = rq_json2['popularity']
+        rank = rq_json2['rank']
+        duration = rq_json2['duration']
+        rating = rq_json2['rating']
+        premiered = rq_json2['premiered']
+        favorites = rq_json2['favorites']
+        scored_by = rq_json2['scored_by']
+        score = rq_json2['score']
+        #anime formatting output
+        anime_picture = rq_json2['image_url']
+        embed = discord.Embed(title="Title: {}".format(
+            query), description=title_en+":"+title_jp, color=0xDEADBF)
+        embed.add_field(name="Type", value=anime_type)
+        embed.add_field(name="Status", value=status)
+        embed.add_field(name="Members", value=members)
+        embed.add_field(name="Popularity", value=popularity)
+        embed.add_field(name="Rank", value=rank)
+        embed.add_field(name="Favorites", value=favorites)
+        embed.add_field(name="Score", value=score)
+        embed.add_field(name="Scored By", value=scored_by)
+        embed.add_field(name="Aired From", value=aired_from)
+        embed.add_field(name="Rating", value=rating)
+        embed.add_field(name="Duration", value=duration)
+        embed.add_field(name="Premiered", value=premiered)
+        embed.set_thumbnail(url=anime_picture)
+        await bot.say(embed=embed)
+        await bot.say("**Summary**: {}".format(summary))
+	
+	
+	
+@bot.command(pass_context=True)
+async def randomshow(ctx):
+    url = 'https://tv-v2.api-fetch.website/random/show'
+    r = rq.get(url).text
+    r_json = json.loads(r)
+    name = r_json['title']
+    year = r_json['year']
+    img = r_json['images']['poster']
+    await bot.say("**Name**: {}\n**Year**: {}\n**Poster**: {}".format(name, year, img))	
+	
+		
+
 
 
 

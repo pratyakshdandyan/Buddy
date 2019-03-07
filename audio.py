70a5ed73ec1c483eb6517c53c985467895c0fbd1
pratyakshdandyan <44299452+pratyakshdandyan@users.noreply.github.com>
2019-03-07 17:03:30 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -435,7 +435,7 @@ async def on_member_join(member):
 @bot.event
 async def on_member_remove(member):
     for channel in member.server.channels:
-        if channel.name == 'welcome':
+        if channel.name == 'leave':
             r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
             embed = discord.Embed(title=f'{member.name} just left {member.server.name}', description='Bye bye 👋! We will miss you 😢', color = discord.Color((r << 16) + (g << 8) + b))
             embed.add_field(name='__User left__', value='**Hope you will be back soon 😕.**', inline=True)
